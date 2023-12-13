5d38afce1831abdca86aebf3ff20f6afaa609006
KosmoMoustache <readyfoxysh@gmail.com>
2023-12-13 02:32:32 +0100

    refactor

--- src/main/java/net/kosmo/nowplaying/mixin/MixinToastManagerEntry.java
+++ src/main/java/net/kosmo/nowplaying/mixin/MixinToastManagerEntry.java
@@ -8,7 +8,6 @@ import net.minecraft.client.toast.ToastManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
