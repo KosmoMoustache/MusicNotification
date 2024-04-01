6b17ead16a2ed09d583b4136e3a83676d32651aa
KosmoMoustache <yanislapo@gmail.com>
2024-04-01 02:08:34 +0200

    create utils pkg

--- src/main/java/net/kosmo/music/utils/ModConfig.java
+++ src/main/java/net/kosmo/music/utils/ModConfig.java
@@ -1,14 +1,16 @@
-package net.kosmo.music;
+package net.kosmo.music.utils;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import net.kosmo.music.ClientMusic;
 
 @Config(name = ClientMusic.MOD_ID)
 public class ModConfig implements ConfigData {
     public boolean HIDE_AUTHOR = false;
     public boolean SHOW_SOUNDTRACK_NAME = false;
     public boolean ROTATE_ALBUM_COVER = true;
+    public boolean SHOW_TITLE_SCREEN_BUTTON = true;
 
     @ConfigEntry.Gui.Tooltip
     @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
