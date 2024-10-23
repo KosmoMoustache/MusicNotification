4cf53856364f07ad2ee47ffef5d6be77c0179306
KosmoMoustache <34519077+kosmomoustache@users.noreply.github.com>
2024-10-23 02:28:51 +0200

    Update to 1.21.2

--- common/src/main/java/net/kosmo/music/impl/toast/MusicToast.java
+++ common/src/main/java/net/kosmo/music/impl/toast/MusicToast.java
@@ -2,9 +2,11 @@ package net.kosmo.music.impl.toast;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.kosmo.music.impl.*;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.toasts.Toast;
-import net.minecraft.client.gui.components.toasts.ToastComponent;
+import net.minecraft.client.gui.components.toasts.ToastManager;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.CommonColors;
@@ -23,33 +25,14 @@ public class MusicToast implements Toast {
     private Component author;
     private Component albumName;
     private AlbumCover albumCover;
+    private Toast.Visibility visibility;
 
     public MusicToast(MusicManager.Music music) {
-        this.title = Component.literal(music.getTitle());
-        this.author = Component.literal(music.getAuthor());
-        this.albumName = Component.literal(music.getAlbumName());
-        this.albumCover = music.albumCover;
-        this.justUpdated = true;
-    }
-
-    public static void show(ToastComponent manager, MusicManager.Music music) {
-        if (!Helper.isVolumeZero()) {
-            return;
-        }
-
-        MusicToast musicToast = manager.getToast(MusicToast.class, Type.DEFAULT);
-        if (musicToast == null) {
-            ClientMusic.LOGGER.debug("Showing toast for {}", music.identifier);
-            manager.addToast(new MusicToast(music));
-        } else {
-            ClientMusic.LOGGER.debug("Setting toast content: {} {}", music.identifier, musicToast);
-            musicToast.setContent(music);
-        }
-
-        ClientMusic.musicHistory.addMusic(music);
+        this.setContent(music);
     }
 
     public void setContent(MusicManager.Music music) {
+        this.visibility = Visibility.SHOW;
         this.title = Component.literal(music.getTitle());
         this.author = Component.literal(music.getAuthor());
         this.albumName = Component.literal(music.getAlbumName());
@@ -58,7 +41,22 @@ public class MusicToast implements Toast {
     }
 
     @Override
-    public Toast.@NotNull Visibility render(GuiGraphics context, ToastComponent manager, long startTime) {
+    public @NotNull Visibility getWantedVisibility() {
+        return this.visibility;
+    }
+
+    public void update(ToastManager toastManager, long l) {
+        if (!Helper.isVolumeZero()) {
+            this.visibility = Visibility.HIDE;
+            return;
+        }
+
+        this.visibility = (double) (l - this.startTime) >= 5000.0 * toastManager.getNotificationDisplayTimeMultiplier() ? Visibility.HIDE : Visibility.SHOW;
+    }
+
+
+    @Override
+    public void render(GuiGraphics guiGraphics, Font font, long startTime) {
         if (rotation >= 360) rotation = 0;
         rotation += 1;
         if (this.justUpdated) {
@@ -67,34 +65,33 @@ public class MusicToast implements Toast {
         }
 
         if (ClientMusic.config.SHOW_ALBUM_NAME) {
-            context.blitSprite(TEXTURE_EXTENDED, 0, 0, this.width(), this.height());
+            guiGraphics.blitSprite(RenderType::guiTextured, TEXTURE_EXTENDED, 0, 0, this.width(), this.height());
         } else {
-            context.blitSprite(TEXTURE, 0, 0, this.width(), this.height());
+            guiGraphics.blitSprite(RenderType::guiTextured, TEXTURE, 0, 0, this.width(), this.height());
         }
 
         // Make the icon rotate
-        context.pose().pushPose();
+        guiGraphics.pose().pushPose();
         if (ClientMusic.config.ROTATE_ALBUM_COVER) {
-            PoseStack matrices = context.pose();
+            PoseStack matrices = guiGraphics.pose();
             matrices.translate(0, 0, 0);
             matrices.translate(16, 16, 0);
             matrices.mulPose(new Quaternionf().rotateLocalZ((float) Math.toRadians(rotation)));
             matrices.translate(-16, -16, 0);
             matrices.translate(-0, -0, 0);
         }
-        this.albumCover.drawAlbumCover(context, 6, 6);
-        context.pose().popPose();
+        this.albumCover.drawAlbumCover(guiGraphics, 6, 6);
+        guiGraphics.pose().popPose();
 
-        context.drawString(manager.getMinecraft().font, this.title, 30, 7, ClientMusic.isDarkModeEnabled ? 0xff75ff : -11534256, false);
+        guiGraphics.drawString(font, this.title, 30, 7, ClientMusic.isDarkModeEnabled ? 0xff75ff : -11534256, false);
 
         if (ClientMusic.config.SHOW_AUTHOR) {
-            context.drawString(manager.getMinecraft().font, this.author, 30, 18, ClientMusic.isDarkModeEnabled ? -3355444 : CommonColors.BLACK, false);
+            guiGraphics.drawString(font, this.author, 30, 18, ClientMusic.isDarkModeEnabled ? -3355444 : CommonColors.BLACK, false);
         }
         if (ClientMusic.config.SHOW_ALBUM_NAME) {
-            RenderHelper.drawScrollableText(context, manager.getMinecraft().font, this.albumName, 30, 30, 29, this.width() - 4, 29 + manager.getMinecraft().font.lineHeight, ClientMusic.isDarkModeEnabled ? -3355444 : CommonColors.BLACK, false, context.guiWidth() - 160 + 30, 0, context.guiWidth() - 4, 44);
+            // TODO: Make the mask follow the X of the toast
+            RenderHelper.drawScrollableText(guiGraphics, font, this.albumName, 30, 30, 29, this.width() - 4, 29 + font.lineHeight, ClientMusic.isDarkModeEnabled ? -3355444 : CommonColors.BLACK, false, guiGraphics.guiWidth() - 160 + 30, 0, guiGraphics.guiWidth() - 4, 44);
         }
-
-        return (double) (startTime - this.startTime) >= 5000.0 * manager.getNotificationDisplayTimeMultiplier() ? Visibility.HIDE : Visibility.SHOW;
     }
 
     @Override
