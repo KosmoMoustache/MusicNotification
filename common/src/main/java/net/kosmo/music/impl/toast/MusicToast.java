88dec9fdc256a60e27b7ed669ea8eecf9158cc32
KosmoMoustache <34519077+KosmoMoustache@users.noreply.github.com>
2024-12-15 23:29:35 +0100

    1.21.4 v2.3.0 (#63)
    * Remove soundtrack key from musics.json
    
    * Fix album name not showing
    
    * Draw scrollable text for title & author
    
    * Update test packs
    
    * Bump version 2.3.0

--- common/src/main/java/net/kosmo/music/impl/toast/MusicToast.java
+++ common/src/main/java/net/kosmo/music/impl/toast/MusicToast.java
@@ -83,14 +83,13 @@ public class MusicToast implements Toast {
         this.albumCover.drawAlbumCover(guiGraphics, 6, 6);
         guiGraphics.pose().popPose();
 
-        guiGraphics.drawString(font, this.title, 30, 7, ClientMusic.isDarkModeEnabled ? 0xff75ff : -11534256, false);
+        RenderHelper.drawScrollableText(guiGraphics, font, this.title, 30, 30, 7, this.width() - 4, 7 + font.lineHeight, ClientMusic.isDarkModeEnabled ? 0xff75ff : -11534256, false, 30, 0, this.width() + 4, this.height());
 
         if (ClientMusic.config.SHOW_AUTHOR) {
-            guiGraphics.drawString(font, this.author, 30, 18, ClientMusic.isDarkModeEnabled ? -3355444 : CommonColors.BLACK, false);
+            RenderHelper.drawScrollableText(guiGraphics, font, this.author, 30, 30, 18, this.width() - 4, 18 + font.lineHeight, ClientMusic.isDarkModeEnabled ? -3355444 : CommonColors.BLACK, false, 30, 0, this.width() + 4, this.height());
         }
         if (ClientMusic.config.SHOW_ALBUM_NAME) {
-            // TODO: Make the mask follow the X of the toast
-            RenderHelper.drawScrollableText(guiGraphics, font, this.albumName, 30, 30, 29, this.width() - 4, 29 + font.lineHeight, ClientMusic.isDarkModeEnabled ? -3355444 : CommonColors.BLACK, false, guiGraphics.guiWidth() - 160 + 30, 0, guiGraphics.guiWidth() - 4, 44);
+            RenderHelper.drawScrollableText(guiGraphics, font, this.albumName, 30, 30, 29, this.width() - 4, 29 + font.lineHeight, ClientMusic.isDarkModeEnabled ? -3355444 : CommonColors.BLACK, false, 30, 0, this.width() + 4, this.height());
         }
     }
 
