90fd1390fb4ec6742abfd740968d694f2c037c02
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-09-30 03:06:43 +0200

    Add initial flake configuration files

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "flake java gnu git";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      packages = with nixpkgs.legacyPackages.x86_64-linux; [
+        pkgs.gnumake
+        pkgs.git
+        pkgs.zulu21
+      ];
+    };
+  };
+}
