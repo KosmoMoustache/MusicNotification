5ee4148c5751aabefb842ae45d9a62096070c2ec
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-11-11 03:20:41 +0100

    PlatformNeoForge

---
+++ neoforge/src/main/java/net/kosmo/music/neoforge/PlatformNeoForge.java
@@ -0,0 +1,27 @@
+package net.kosmo.music.neoforge;
+
+import net.kosmo.music.PlatformHelper;
+import net.kosmo.music.neoforge.notification.GuiCompactLayer;
+import net.kosmo.music.resource.TrackData;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.fml.loading.LoadingModList;
+
+public class PlatformNeoForge extends PlatformHelper {
+	@Override
+	public String getModName(ResourceLocation location) {
+		String namespace = location.getNamespace();
+		namespace = LoadingModList.get().getModFileById(namespace).moduleName();
+		return namespace;
+	}
+
+	@Override
+	public void setCompactNotification(TrackData trackData, int time) {
+		GuiCompactLayer layer = GuiCompactLayer.getInstance();
+		if (layer != null && trackData != null) {
+			Component message = Component.literal(trackData.title().getString() + " - " + trackData.author().getString());
+			layer.setNotification(message, time);
+		}
+	}
+
+}
