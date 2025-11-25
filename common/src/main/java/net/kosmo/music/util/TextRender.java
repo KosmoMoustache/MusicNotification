b6c117477badd7d3ab82068a5ca2c2310919eb1c
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-11-25 23:59:54 +0100

    refactor: update drawScrollableText method parameters

--- common/src/main/java/net/kosmo/music/util/TextRender.java
+++ common/src/main/java/net/kosmo/music/util/TextRender.java
@@ -8,33 +8,30 @@ import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.util.Mth;
 
 public class TextRender {
-    /**
-     * Draw a scrollable text
-     */
-    public static void drawScrollableText(GuiGraphics context, Font textRenderer, Component text, int centerX, int startX, int startY, int endX, int endY, int color, boolean shadow) {
-        drawScrollableText(context, textRenderer, text, centerX, startX, startY, endX, endY, color, shadow, startX, startY, endX, endY);
-    }
+	/**
+	 * Draw a scrollable text
+	 */
+	public static void drawScrollableText(GuiGraphics context, Font textRenderer, Component text, int centerX, int minX, int minY, int maxX, int maxY, int color, boolean shadow) {
+		int i = textRenderer.width(text);
+		int j = (minY + maxY - textRenderer.lineHeight) / 2 + 1;
+		int k = maxX - minX;
+		if (i > k) {
+			int l = i - k;
+			double d = (double) Util.getMillis() / 1000.0;
+			double e = Math.max((double) l * 0.5, 3.0);
+			double f = Math.sin(Math.PI / 2D * Math.cos((Math.PI * 2D) * d / e)) / 2.0 + 0.5;
+			double g = Mth.lerp(f, 0.0, l);
 
-    public static void drawScrollableText(GuiGraphics context, Font textRenderer, Component text, int centerX, int startX, int startY, int endX, int endY, int color, boolean shadow, int clipAreaX1, int clipAreaY1, int clipAreaX2, int clipAreaY2) {
-        int i = textRenderer.width(text);
-        int j = (startY + endY - textRenderer.lineHeight) / 2 + 1;
-        int k = endX - startX;
-        if (i > k) {
-            int l = i - k;
-            double d = (double) Util.getMillis() / 1000.0;
-            double e = Math.max((double) l * 0.5, 3.0);
-            double f = Math.sin(1.5707963267948966 * Math.cos(Math.PI * 2 * d / e)) / 2.0 + 0.5;
-            double g = Mth.lerp(f, 0.0, (double) l);
+			context.enableScissor(minX, minY, maxX, maxY);
+//			context.fill(minX, minY, maxX, maxY, CommonColors.RED);
+			context.drawString(textRenderer, text.getVisualOrderText(), minX - (int) g, j, color, shadow);
+			context.disableScissor();
+		} else {
+//			context.fill(0, 0, 1, 10, CommonColors.RED);
+			int l = Mth.clamp(centerX, minX + i / 2, maxX - i / 2);
 
-            context.enableScissor(clipAreaX1, clipAreaY1, clipAreaX2, clipAreaY2);
-//            context.fill(clipAreaX1,clipAreaY1,clipAreaX2,clipAreaY2, CommonColors.RED);
-            context.drawString(textRenderer, text.getVisualOrderText(), startX - (int) g, j, color, shadow);
-            context.disableScissor();
-        } else {
-            int l = Mth.clamp(centerX, startX + i / 2, endX - i / 2);
-
-            FormattedCharSequence orderedText = text.getVisualOrderText();
-            context.drawString(textRenderer, orderedText, l - textRenderer.width(orderedText) / 2, j, color, shadow);
-        }
-    }
+			FormattedCharSequence orderedText = text.getVisualOrderText();
+			context.drawString(textRenderer, orderedText, l - textRenderer.width(orderedText) / 2, j, color, shadow);
+		}
+	}
 }
