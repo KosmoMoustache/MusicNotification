a66e35b2b968b6ada1c1c59a8a261f55f0bd9041
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-11-26 02:24:09 +0100

    feat: add bug report template and update build workflows

---
+++ .github/scripts/parse-gradle-properties.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#!/usr/bin/env bash
+
+version=${1:-}
+
+parse_properties_file() {
+    local file=$1
+    while IFS='=' read -r key value; do
+        key=$(echo "$key" | awk '{$1=$1;print}')
+        value=$(echo "$value" | awk '{$1=$1;print}')
+
+        if [[ -z "$key" || "$key" =~ ^# || "$key" == "org.gradle.jvmargs" ]]; then
+            continue
+        fi
+
+        key=$(echo "$key" | tr '[:lower:]' '[:upper:]' | tr -c '[:alnum:]' '_')
+        key=$(echo "$key" | sed 's/_$//')
+
+        echo "${key}=${value}"
+        echo "${key}=${value}" >> "$GITHUB_OUTPUT"
+    done < "$file"
+}
+
+parse_properties_file gradle.properties
+
+if [[ -n "$version" ]]; then
+    parse_properties_file "versions/${version}/gradle.properties"
+fi
--- img/icon/clean.bat
+++
@@ -1,3 +0,0 @@
-del result\*_step1.png
-del result\*_step2.png
-del result\*_final.png
