589bb32fefe920781e589b671045a4e4726588bf
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-12-03 04:40:33 +0100

    refactor: rearrange code

--- common/src/main/java/net/kosmo/music/mixin/MusicManagerAccessor.java
+++ common/src/main/java/net/kosmo/music/mixin/MusicManagerAccessor.java
@@ -3,16 +3,15 @@ package net.kosmo.music.mixin;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.annotation.Nullable;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.MusicManager;
-import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(MusicManager.class)
 public interface MusicManagerAccessor {
 
 	@Accessor
-	void setCurrentMusic(SoundInstance soundInstance);
-
-	@Accessor
 	@Nullable
 	SoundInstance getCurrentMusic();
+
+	@Accessor
+	void setCurrentMusic(SoundInstance soundInstance);
 }
