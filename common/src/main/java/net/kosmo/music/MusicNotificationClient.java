75afc3cf28183ddfbcfe0dd5732c5f47e1a60432
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-12-15 21:32:44 +0100

    refactor: clean up unused code

--- common/src/main/java/net/kosmo/music/MusicNotificationClient.java
+++ common/src/main/java/net/kosmo/music/MusicNotificationClient.java
@@ -3,8 +3,6 @@ package net.kosmo.music;
 import net.kosmo.music.config.Config;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.sounds.SoundInstance;
-import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,14 +12,7 @@ public final class MusicNotificationClient {
 	public static final Logger LOGGER = LoggerFactory.getLogger("MusicNotification");
 
 	public static PlatformHelper PLATFORM_HELPER;
-
-
-	@Nullable
-	public static SoundInstance currentlyPlaying;
-	public static boolean initialized;
-
-//    public static String lastSong;
-//    public static String currentSong;
+	public static @Nullable SoundInstance currentlyPlaying;
 
 	public static void init(PlatformHelper platformHelper) {
 		LOGGER.info("Music Notification initialized");
@@ -29,30 +20,11 @@ public final class MusicNotificationClient {
 		MusicNotificationClient.PLATFORM_HELPER = platformHelper;
 	}
 
-	public static void tick(Minecraft minecraft) {
-		tick();
-	}
-
 	public static void tick() {
+		tick(Minecraft.getInstance());
+	}
+
+	public static void tick(Minecraft minecraft) {
 		KeyBinding.tick();
-
-//        currentSong = minecraft.getMusicManager().getCurrentMusicTranslationKey();
-//        if (currentSong != null && !currentSong.equals(lastSong)) {
-//            lastSong = currentSong;
-//            LOGGER.info(currentSong, getNowPlayingString(currentSong).getString());
-//        }
-
-//        minecraft.getSoundManager().getAvailableSounds();
-	}
-
-	static Component getNowPlayingString(String currentSong) {
-		return currentSong == null ? Component.empty() : Component.translatable(currentSong.replace("/", "."));
-	}
-
-	static ResourceLocation id(String path) {
-		return ResourceLocation.fromNamespaceAndPath(
-			MOD_ID,
-			path
-		);
 	}
 }
