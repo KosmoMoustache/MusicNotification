7e8fa669bbf95e2436e695a4a7800b4f01351907
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-12-15 21:33:22 +0100

    fix: import Mixin

--- common/src/main/java/net/kosmo/music/mixin/MusicManagerAccessor.java
+++ common/src/main/java/net/kosmo/music/mixin/MusicManagerAccessor.java
@@ -3,6 +3,7 @@ package net.kosmo.music.mixin;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.annotation.Nullable;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.sounds.MusicManager;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(MusicManager.class)
