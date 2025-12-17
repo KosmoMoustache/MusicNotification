ab419a82ef837f29c4bc034b45ce07b55325e68d
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-12-17 12:53:43 +0100

    chore: update net.neoforged.moddev and fabric-loom plugin versions

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    id("dev.kikugie.stonecutter")
+	id("dev.kikugie.stonecutter")
 
-    id("net.neoforged.moddev") version "2.0.97" apply false
-    id("fabric-loom") version "1.11-SNAPSHOT" apply false
+	id("net.neoforged.moddev") version "2.0.126" apply false
+	id("fabric-loom") version "1.11-SNAPSHOT" apply false
 //    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
 }
 stonecutter active "1.21.7" /* [SC] DO NOT EDIT */
