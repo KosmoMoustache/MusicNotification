8e7d44cae73a4e280253a8dd014b20308514ba6c
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-12-17 12:02:23 +0100

    chore: update fletching-table and stonecutter plugin versions

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 	`multiloader-loader`
 	kotlin("jvm") version "2.2.0"
 	id("com.google.devtools.ksp") version "2.2.0-2.0.2"
-	id("dev.kikugie.fletching-table.fabric") version "0.1.0-alpha.18"
+	id("dev.kikugie.fletching-table.fabric") version "0.1.0-alpha.22"
 }
 
 fletchingTable {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,33 +4,33 @@ gradle.startParameter.isBuildCacheEnabled = !isCi
 gradle.startParameter.isConfigureOnDemand = !isCi
 
 pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-        maven("https://maven.fabricmc.net/")
-        maven("https://maven.neoforged.net/releases/")
-        maven("https://maven.minecraftforge.net")
-        maven("https://maven.kikugie.dev/snapshots")
-        maven("https://maven.kikugie.dev/releases")
-    }
+	repositories {
+		mavenCentral()
+		gradlePluginPortal()
+		maven("https://maven.fabricmc.net/")
+		maven("https://maven.neoforged.net/releases/")
+		maven("https://maven.minecraftforge.net")
+		maven("https://maven.kikugie.dev/snapshots")
+		maven("https://maven.kikugie.dev/releases")
+	}
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.7.6"
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+	id("dev.kikugie.stonecutter") version "0.7.11"
+	id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 val commonVersions =
-    providers.gradleProperty("stonecutter_enabled_common_versions").orNull?.split(",")?.map { it.trim() } ?: emptyList()
+	providers.gradleProperty("stonecutter_enabled_common_versions").orNull?.split(",")?.map { it.trim() } ?: emptyList()
 val fabricVersions =
-    providers.gradleProperty("stonecutter_enabled_fabric_versions").orNull?.split(",")?.map { it.trim() } ?: emptyList()
+	providers.gradleProperty("stonecutter_enabled_fabric_versions").orNull?.split(",")?.map { it.trim() } ?: emptyList()
 val neoforgeVersions =
-    providers.gradleProperty("stonecutter_enabled_neoforge_versions").orNull?.split(",")?.map { it.trim() }
-        ?: emptyList()
+	providers.gradleProperty("stonecutter_enabled_neoforge_versions").orNull?.split(",")?.map { it.trim() }
+		?: emptyList()
 val dists = mapOf(
-    "common" to commonVersions,
-    "fabric" to fabricVersions,
-    "neoforge" to neoforgeVersions
+	"common" to commonVersions,
+	"fabric" to fabricVersions,
+	"neoforge" to neoforgeVersions
 )
 val uniqueVersions = dists.values.flatten().distinct()
 
@@ -38,15 +38,15 @@ stonecutter {
 //    kotlinController = true
 //    centralScript = "build.gradle.kts"
 
-    create(rootProject) {
-        versions(*uniqueVersions.toTypedArray())
+	create(rootProject) {
+		versions(*uniqueVersions.toTypedArray())
 
-        dists.forEach { (branchName, branchVersions) ->
-            branch(branchName) {
-                versions(*branchVersions.toTypedArray())
-            }
-        }
-    }
+		dists.forEach { (branchName, branchVersions) ->
+			branch(branchName) {
+				versions(*branchVersions.toTypedArray())
+			}
+		}
+	}
 }
 
 rootProject.name = "MusicNotification"
