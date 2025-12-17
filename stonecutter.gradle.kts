3f35cc943bc01e8d18ed700d2a97c41a4f068832
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-12-17 22:41:44 +0100

    fix: update fabric-loom version to 1.14-SNAPSHOT

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	id("dev.kikugie.stonecutter")
 
 	id("net.neoforged.moddev") version "2.0.126" apply false
-	id("fabric-loom") version "1.11-SNAPSHOT" apply false
+	id("fabric-loom") version "1.14-SNAPSHOT" apply false
 //    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
 }
 stonecutter active "1.21.9" /* [SC] DO NOT EDIT */
