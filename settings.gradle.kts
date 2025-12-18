93fd6d95a614dd79865b6e41c983b418161cd777
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2025-12-18 02:56:09 +0100

    chore: update stonecutter to version 0.8-beta.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ pluginManagement {
 }
 
 plugins {
-	id("dev.kikugie.stonecutter") version "0.7.11"
+	id("dev.kikugie.stonecutter") version "0.8-beta.2"
 	id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
