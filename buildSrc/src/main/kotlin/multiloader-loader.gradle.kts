b7346c42ebff826274da74d04604c407ed609139
KosmoMoustache <34519077+KosmoMostache@users.noreply.github.com>
2026-04-20 00:53:38 +0200

    fix: include only the right access widener file in the jar

--- buildSrc/src/main/kotlin/multiloader-loader.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader-loader.gradle.kts
@@ -19,13 +19,20 @@ dependencies {
 }
 
 tasks {
-	compileJava {
+	named<JavaCompile>("compileJava") {
 		dependsOn(commonJava)
 		source(commonJava)
 	}
+	named<ProcessResources>("processResources") {
+//		dependsOn(commonResources)
 
-	processResources {
-		dependsOn(commonResources)
-		from(commonResources)
+
+		// Only include the right aw file
+		from(commonResources) {
+			exclude("**/*.aw")
+		}
+		from(commonResources) {
+			include("${mod.aw_version}.aw")
+		}
 	}
 }
